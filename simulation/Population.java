347baa15f65431d92465edf2e96e335883db68c4
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-04 22:43:45 +0200

    Update Population.java

--- simulation/Population.java
+++ simulation/Population.java
@@ -1,32 +1,32 @@
 package simulation;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Population {
 	
 	private DNA type;
 	private List<DNA> pool = new ArrayList<DNA>();
+	
 	private Consumer<List<DNA>> onUpdate;
+	private Function<List<DNA>, List<Double> getFitness;
 	
 	private double mutationChange = 0.01;
 	private double selectionPressure = 0.01;
 	private double generationRefreshFactor = 2;
 	
-	public Population(DNA type, int poolSize) {
+	public Population(Function<List<DNA>, List<Double> getFitness, DNA type, int poolSize) {
 		
 		this.type = type;
+		this.getFitness = getFitness;
 		
 		initPool(poolSize);
-		
-	}
-	
-	public Population(DNA type, int poolSize, Consumer<List<DNA<T>>> onUpdate) {
-		
-		this(type, poolSize);
-		
-		this.onUpdate = onUpdate;
 		
 	}
 	
@@ -58,9 +58,44 @@ public class Population {
 		
 	}
 	
+	public void doGenerations(int n) {
+		
+		for (int i = 0; i < n; i++) {
+			update();
+		}
+		
+	}
+	
+	public void doGenerationsAsync(int n, Consumer<Population> onComplete) {
+		
+		new Thread() {
+			@Override
+			public void run() {
+				doGenerations(n);
+				onComplete.accept(Population.this);
+			}
+		}.start();
+		
+	}
+	
+	public Future<Population> doGenerationsAsync(int n) {
+		
+		ExecutorService es = Executors.newSingleThreadExecutor();
+		return es.submit(new Callable<Population>() {
+			@Override
+			public Population call() {
+				doGenerations(n);
+				return Population.this;
+			}
+		});
+		
+	}
+	
 	private void select() {
 		
-		//TODO create weighted list
+		List<Double> fitnesses = getFitness.apply(new ArrayList<DNA>(pool));
+			
+		//TODO create weighted pool
 		
 		for (int i = 0; i < generationRefreshFactor; i++) {
 			//TODO remove random child from said list
