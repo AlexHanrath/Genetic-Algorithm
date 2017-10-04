6534bc5985cccfd488b795f884b4ca63f9ba1515
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-04 22:14:37 +0200

    Update DNA.java

--- simulation/DNA.java
+++ simulation/DNA.java
@@ -1,27 +1,48 @@
 package simulation;
 
-public abstract class DNA<T> implements Cloneable {
+public class DNA implements Cloneable {
 	
-	private T[] genes;
+	private int[] genes;
+	private int min;
+	private int max;
 	
 	public abstract void update();
 	
 	public void mutate() {
 		
-		//TODO
+		int index = Math.round(Math.random()*(genes.length-1));
+		mutateGene(index);
 		
 	}
 	
-	@Override
-	public DNA<T> clone() {
-		DNA<T> copy = new DNA<T>(genes.length);
-		copy.genes = genes;
+	private void mutateGene(int index) {
+		
+		genes[index] = Math.round(Math.random()*(max-min))+min;
+		
+	}
+	
+	public DNA randomClone() {
+		DNA copy = clone();
+		for (int i = 0; i < genes.length) {
+			mutateGene(i);
+		}
 		return copy;
 	}
 	
-	public DNA(int nGenes) {
+	@Override
+	public DNA clone() {
+		DNA copy = new DNA(genes.length);
+		copy.genes = genes;
+		copy.min = min;
+		copy.max = max;
+		return copy;
+	}
+	
+	public DNA(int nGenes, int min, int max) {
 		
-		genes = new T[nGenes];
+		this.min = min;
+		this.max = max;
+		genes = new int[nGenes];
 			
 	}
 	
