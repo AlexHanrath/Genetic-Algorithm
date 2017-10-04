9b7ec34a3322e9971a03780082d4484883725c0c
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-04 22:03:30 +0200

    Update DNA.java

--- simulation/DNA.java
+++ simulation/DNA.java
@@ -1,8 +1,28 @@
 package simulation;
 
-public abstract class DNA implements Cloneable {
+public abstract class DNA<T> implements Cloneable {
+	
+	private T[] genes;
 	
 	public abstract void update();
-	public abstract void mutate();
+	
+	public void mutate() {
+		
+		//TODO
+		
+	}
+	
+	@Override
+	public DNA<T> clone() {
+		DNA<T> copy = new DNA<T>(genes.length);
+		copy.genes = genes;
+		return copy;
+	}
+	
+	public DNA(int nGenes) {
+		
+		genes = new T[nGenes];
+			
+	}
 	
 }
