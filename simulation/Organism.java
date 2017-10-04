2001f6cddfa364389ac8d91e189c9e5452c0184f
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-04 21:54:35 +0200

    Update Organism.java

--- simulation/Organism.java
+++ simulation/Organism.java
@@ -2,7 +2,15 @@ package simulation;
 
 public abstract class Organism {
 	
+	private int[] genome;
+	
 	public abstract void update() {
+		
+		//TODO
+		
+	}
+	
+	public abstract void mutate() {
 		
 		//TODO
 		
