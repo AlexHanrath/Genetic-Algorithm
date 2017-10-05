9f655912afd6beaad586f6a433bbaefebd566bdd
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-05 20:15:02 +0200

    Update Test.java

--- testing/Test.java
+++ testing/Test.java
@@ -1,11 +1,44 @@
 package testing;
 
+import java.util.function.Function;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Test {
-
-  public static void main(String[] args) {
-  
-  
-  
-  }
-
+	
+	public static class TextDNA extends DNA {
+		
+		public static Function<List<DNA>, List<Double>> getFitness = dnas -> {
+			
+			//TODO
+			
+		};
+		
+		public TextDNA(int nGenes, double min, double max) {
+			super(nGenes, min, max, 1);
+		}
+		
+		@Override
+		public void update() {}
+		@Override
+		public DNA clone() {
+			TextDNA copy = new TextDNA(genes.length, min, max, step);
+			copy.genes = new double[genes.length];
+			System.arrayCopy(genes, 0, genes.length, copy.genes, 0);
+			return copy;
+		}
+		
+	}
+	
+	public static final String[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "};
+	public static final String target = "hello world";
+	public static final int size = 100;
+	
+	public static void main(String[] args) {
+    		
+		TextDNA template = new TextDNA(11, 0, 26);
+    		Population p = new Population(TextDNA.getFitness, template, size)
+    		
+	}
+	
 }
