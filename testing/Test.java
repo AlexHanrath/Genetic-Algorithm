ed6047f8734659b83359340dabdc1a5183154844
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-05 20:42:30 +0200

    Update Test.java

--- testing/Test.java
+++ testing/Test.java
@@ -37,6 +37,15 @@ public class Test {
 			return copy;
 		}
 		
+		@Override
+		public String toString() {
+			String res = "";
+			for (int i = 0; i < genes.length; i++) {
+				res += charMap[(int) genes[i]];
+			}
+			return res;
+		}
+		
 	}
 	
 	public static final char[] charMap = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
@@ -53,7 +62,7 @@ public class Test {
 		List<DNA> pool = p.getPool();
 		
 		for (DNA dna : pool) {
-			System.out.println(dna.toString());
+			System.out.println(((TextDNA) dna).toString());
 		}
     		
 	}
