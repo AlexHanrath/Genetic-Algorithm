9df92fca933bbd131bb2266f4b4245b4e38a23cd
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-05 20:31:27 +0200

    Update Test.java

--- testing/Test.java
+++ testing/Test.java
@@ -3,6 +3,7 @@ package testing;
 import java.util.function.Function;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import simulation.*;
 
@@ -10,15 +11,17 @@ public class Test {
 	
 	public static class TextDNA extends DNA {
 		
-		public static Function<List<DNA>, List<Double>> getFitness = dnas -> dnas.parallelStream().map(dna -> {
+		public static Function<List<DNA>, List<Double>> getFitness = dnas -> dnas.parallelStream().map(dna -> dna.countMatch(target)).collect(Collectors.toList());
+		
+		public double countMatch(String target) {
 			double count = 0;
-			for (int i = 0; i < dna.genes.length; i++) {
-				if (charMap[(int) dna.genes[i]] == target.charAt(i)) {
+			for (int i = 0; i < genes.length; i++) {
+				if (charMap[(int) genes[i]] == target.charAt(i)) {
 					count++;
 				}
 			}
-			return cuont;
-		}).collect(Collectors.toList());
+			return count;
+		}
 		
 		public TextDNA(int nGenes, double min, double max) {
 			super(nGenes, min, max, 1);
@@ -39,11 +42,15 @@ public class Test {
 	public static final char[] charMap = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
 	public static final String target = "hello world";
 	public static final int size = 100;
+	public static final int nGenerations = 1000;
 	
 	public static void main(String[] args) {
     		
 		TextDNA template = new TextDNA(11, 0, 26);
-    		Population p = new Population(TextDNA.getFitness, template, size)
+    		Population p = new Population(TextDNA.getFitness, template, size);
+		p.doGenerations(nGenerations);
+		
+		//TODO Output
     		
 	}
 	
