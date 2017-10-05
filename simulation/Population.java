ea6a2a09f13d7289ea20bc9118aa6e5fdaabc29c
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-05 20:37:02 +0200

    Update Population.java

--- simulation/Population.java
+++ simulation/Population.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.Map.Entry;
 
 public class Population {
 	
