aca94e58145037f795741474dfec035c81cc8303
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-05 18:11:27 +0200

    Update WeightedSelectionList.java

--- simulation/WeightedSelectionList.java
+++ simulation/WeightedSelectionList.java
@@ -1,7 +1,54 @@
 package simulation;
 
-public class WeightedSelectionList {
+import java.util.Map;
+import java.util.HashMap;
+
+public class WeightedSelectionList<T> {
 	
+	private Map<T, Double> data = new HashMap<T, Double>();
+	private double total = 0;
 	
+	public void add(double weight, T elem) {
+		
+		remove(elem);
+		
+		data.put(weight, elem);
+		total += weight;
+		
+	}
+	
+	public void remove(T elem) {
+		
+		if (data.containsKey(elem)) {
+			
+			total -= data.get(elem);
+			data.remove(elem);
+			
+		}
+		
+	}
+	
+	public T removeRandom() {
+		
+		double v = Math.random()*total;
+		T sel = null;
+		
+		for (Entry<T, Double> entry : data.getEntries()) {
+			
+			v -= entry.getValue();
+			
+			if (v <= 0) {
+				
+				sel = entry.getKey();
+				break;
+				
+			}
+			
+		}
+		
+		remove(sel);
+		return sel;
+		
+	}
 	
 }
